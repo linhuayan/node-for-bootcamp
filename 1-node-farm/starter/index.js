856b1c2d8e8d0e39a62937d3d640ce53adb85937
v_huaylin <v_huaylin@tencent.com>
2023-05-05 17:39:54 +0800

    feat: product template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 //////////////////////////
 // FILES
@@ -33,11 +33,29 @@ const url = require('url');
 
 /////////////////////////////////
 // SERVER
+const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end('Hello from the server!');
-})
+	console.log(req.url);
+	const pathName = req.url;
+	if (pathName === "/" || pathName === "/overview") {
+		res.end("This is the OVERVIEW");
+	} else if (pathName === "/product") {
+		res.end("This is the PRODUCT");
+	} else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+		res.end("<h1>Page not found!</h1>");
+	}
+});
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
-})
+server.listen(8008, "127.0.0.1", () => {
+	console.log("Listening to requests on port 8008");
+});
