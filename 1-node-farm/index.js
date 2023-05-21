adcdac5f7e47a252bb5fd981f87355c00ac43f8f
v_huaylin <v_huaylin@tencent.com>
2023-05-21 22:55:16 +0800

    feat: 增加nodemon

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 
 //////////////////////////
 // FILES
@@ -34,23 +36,6 @@ const url = require("url");
 /////////////////////////////////
 // SERVER
 
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%ID%}/g, product.id);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-	if (!product.organic) {
-		output = output.replace(/{%NOT_ORGANIC%}/g, ".not-organic");
-	}
-
-  return output;
-};
-
 const tempCard = fs.readFileSync(
 	`${__dirname}/templates/template-card.html`,
 	"utf-8"
@@ -65,18 +50,25 @@ const temOverview = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log(slugs);
+console.log(slugify('Fresh Avocados', {lower: true}));
 const server = http.createServer((req, res) => {
-	console.log(req.url);
-	const pathName = req.url;
+	// console.log(req.url);
+	// const pathName = req.url;
+	const {query, pathname: pathName} = url.parse(req.url, true);
 	// Overview page
 	if (pathName === "/" || pathName === "/overview") {
 		res.writeHead(200, { "Content-type": "text/html" });
 		const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join(''); //最后返回的是一个数组，而我们想要的是html字符串，所以要加join('')
-    const overviewHtml = temOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
+    	const overviewHtml = temOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
 		res.end(overviewHtml);
 		// Product page
 	} else if (pathName === "/product") {
-		res.end("This is the OVERVIEW");
+		res.writeHead(200, {"Content-type": "text/html"});
+		const product = dataObj[query.id];
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output);
 		// API
 	} else if (pathName === "/api") {
 		res.writeHead(200, {
