09169e0a23d0cc209e14a3088e8c1e328f6adcf3
v_huaylin <v_huaylin@tencent.com>
2023-05-30 23:25:41 +0800

    feat: four part

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,23 +2,45 @@ const fs = require('fs');
 const express = require('express');
 const port = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.json());
 // app.get('/', (req, res) =>{
 //   res.status(404).json({message: 'Hello from the server side!', app: 'Natours'});
 // })
 // app.post('/', (req, res) =>{
 //   res.send('You can post to this endpoint...');
 // })
-const tours  = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/api/v1/tours', (req, res) =>{
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
-})
+});
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
