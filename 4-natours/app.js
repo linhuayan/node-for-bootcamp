7515a05041a29995877a44ea8f69fa684c5a8771
v_huaylin <v_huaylin@tencent.com>
2023-05-30 17:27:12 +0800

    feat: the four part start

---
+++ 4-natours/app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const port = process.env.PORT || 3001;
+const app = express();
+// app.get('/', (req, res) =>{
+//   res.status(404).json({message: 'Hello from the server side!', app: 'Natours'});
+// })
+// app.post('/', (req, res) =>{
+//   res.send('You can post to this endpoint...');
+// })
+const tours  = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) =>{
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
