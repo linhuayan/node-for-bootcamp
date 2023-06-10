ad4fead48d2ab848eebdc089e62625a24e1f3193
v_huaylin <v_huaylin@tencent.com>
2023-06-10 22:34:18 +0800

    feat: add request.md

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -105,11 +105,12 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true
     });
     res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour here...>',
+        tour
       },
     });
   } catch (err) {
