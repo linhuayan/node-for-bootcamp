5ced430197cc76c733e249ef6f92a037d2463071
v_huaylin <v_huaylin@tencent.com>
2023-06-27 16:18:33 +0800

    feat: add APIFeature

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModel');
-// const fs = require('fs');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -14,49 +14,49 @@ exports.getAllTours = async (req, res) => {
     // console.log(req.query);
     // BUILD QUERY
     // 1A）Filtering
-    const queryObj = {...req.query}
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el])
+    // const queryObj = {...req.query}
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach(el => delete queryObj[el])
 
-    // 2B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // // 2B) Advanced filtering
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    let query = Tour.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
+
     // 2) Sorting 
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort('-createdAt');
+    // }
 
     // 3) Field limiting 
-    if (req.query.fields) {
-       const fields = req.query.fields.split(',').join(' ');
-       query = query.select(fields);
-    } else {
-      // 减去带下划线v的项
-      query = query.select('-__v');
-    }
+    // if (req.query.fields) {
+    //    const fields = req.query.fields.split(',').join(' ');
+    //    query = query.select(fields);
+    // } else {
+    //   // 减去带下划线v的项
+    //   query = query.select('-__v');
+    // }
 
     // 4）Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-    // page=3&limit10, 1-10, page 1, 11-20, page 2, 21-30, page 3
-    query = query.skip(skip).limit(limit);
+    // // page=3&limit10, 1-10, page 1, 11-20, page 2, 21-30, page 3
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     // EXECUTE QUERY
-    const tours = await query;
-
-    // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
@@ -73,6 +73,30 @@ exports.getAllTours = async (req, res) => {
     })
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([
+      {
+        $match:{ratingsAverage: {$gte: 4.5}}
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage'},
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price'},
+          maxPrice: { $max: '$price'}
+        }
+      }
+    ]);
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
+}
 
 exports.getTour = async (req, res) => {
   // const id = +req.params.id;
@@ -110,23 +134,6 @@ exports.createTour = async (req, res) => {
       message: err
     })
   }
-  
-
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
 };
 
 exports.updateTour = async (req, res) => {
@@ -151,10 +158,7 @@ exports.updateTour = async (req, res) => {
 };
 
 exports.deleteTour = async (req, res) => {
-  // const id = +req.params.id;
-
   try {
-    // const tour = await Tour.deleteOne({id: req.params.id})
     await Tour.findByIdAndDelete(req.params.id);
 
     // 204表示没有内容，不发送任何内容
